0dd5451ab99a5519fa84dbae629fd363f2861f5c
Umwizer <ruthumwizerwa@gmail.com>
2025-07-30 14:16:10 +0200

    creating the calculator workplace

---
+++ calculatorAp/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const [numbers, setNumbers] = useState([0]);
+function App() {
+  return (
+    <div>
+      <button></button>
+    </div>
+  );
+}
+export default App;
