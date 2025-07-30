8b59b581211ad28a7f36a717f2a263fd3812f4c0
Umwizer <ruthumwizerwa@gmail.com>
2025-07-30 14:32:55 +0200

    calculator app

--- calculatorAp/src/App.tsx
+++ calculatorAp/src/App.tsx
@@ -1,10 +1,16 @@
+import * as React from "react";
 import { useState } from "react";
-
-const [numbers, setNumbers] = useState([0]);
-function App() {
+function App(): React.JSX.Element {
+  let [number, setNumber] = useState<string>("");
+  const handleCalc = (num: string): void => {
+    setNumber(number + num);
+  };
   return (
     <div>
-      <button></button>
+      <div>{number || "0"}</div>
+      <button onClick={() => handleCalc("1")}>1</button>
+      <button onClick={() => handleCalc("2")}>2</button>
+      <button onClick={() => handleCalc("3")}>3</button>
     </div>
   );
 }
